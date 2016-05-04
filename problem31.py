25e0c9259b5d778d3a165b56716edfd358e7c9bb
Matúš Košút <matuskosut@gmail.com>
2016-05-04 12:38:52 +0200

    definitely a better way

--- problem31.py
+++ problem31.py
@@ -1,10 +1,11 @@
-count = 0
-for a in range(0, 201, 200):
-    for b in range(a, 201, 100):
-        for c in range(b, 201, 50):
-            for d in range(c, 201, 20):
-                for e in range(d, 201, 10):
-                    for f in range(e, 201, 5):
-                        for g in range(f, 201, 2):
-                            count = count + 1
-print count
+from time import time
+start = time()
+
+arr = [1] + [0] * 200
+
+for x in [1, 2, 5, 10, 20, 50, 100, 200]:
+    for y in range(x, 201):
+        arr[y] += arr[y - x]
+
+print arr[200]
+print "Time: {0} secs".format(time() - start)
